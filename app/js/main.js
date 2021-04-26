579bcc8b417dc32e4338b0409631489a1bef4319
Goto.07 <goto.07@mail.ru>
2021-04-26 12:55:20 +0300

    final

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,38 @@
 $(function () {
 
+  $('.menu__button, .menu a').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
   $(".shop-card__star").rateYo({
     rating: 4,
     starWidth: "15px",
   })
 
+  let slides = 3;
+  if(window.innerWidth > 1240) {
+      slides = 3;
+      window.reload;
+    }
+    else if (window.innerWidth < 1240 && window.innerWidth > 677){
+      slides = 2;
+      console.log("|window.innerWidth");
+    }
+    else if (window.innerWidth < 676){
+      console.log(window.innerWidth);
+      slides = 1;
+    }  
+/**/
+    window.onresize = function( event ) {
+      document.location.reload();
+    };
+    
+  console.log(window.innerWidth);
+  
+
   $('.store-slider').slick({
     arrows: true,
-    slidesToShow: 4,
+    slidesToShow: slides,
     slidesToScroll: 1,
     prevArrow: '<button type="button" class="slick-prev store-slider__button store-slider__button--slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-next store-slider__button store-slider__button--slick-next"></button>',
